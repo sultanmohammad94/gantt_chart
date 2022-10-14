7c64ac0d131000e8292cd10de85e63bbff4bcbae
= <=>
2022-10-14 13:13:40 +0300

    built the task model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    """This class represnts a DXHTML Task."""
+    
+    id = models.AutoField(primary_key = True, editable = False)
+    text = models.CharField(blank = True, max_length = 150)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    duration = models.IntegerField()
+    progress = models.FloatField()
+    parent = models.CharField(max_length = 150)
+    
+    
+    
+class Link(models.Model):
+    id = models.AutoField(primary_key = True, editable = False)
+    source = models.CharField(max_length = 150)
+    target = models.CharField(max_length = 150)
+    type = models.CharField(max_length = 150)
+    lag = models.IntegerField(blank = True, default = 0)
+    
